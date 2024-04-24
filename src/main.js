7720fac67cadfc0401b2db81945bb6fa95b9bdd2
huang <yuan>
2024-04-24 13:05:50 +0800

    初级版本

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+/* 引入 ElementPlus */
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+const app=createApp(App);
+app.use(ElementPlus);
+app.mount('#app');
 
-createApp(App).mount('#app')
